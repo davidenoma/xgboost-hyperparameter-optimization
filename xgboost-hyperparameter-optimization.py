5e3ccffe514022d022718e53ade8723f4e0d554d
David Enoma <davidenoma@gmail.com>
2021-07-06 16:24:02 +0100

    Initial commit

---
+++ xgboost-hyperparameter-optimization.py
@@ -0,0 +1,168 @@
+import pandas as pd
+file = "C:/Users/HP/Downloads/Dataset/Dataset/Training/Features_Variant_1.csv"
+df = pd.read_csv(file, header=None)
+df.sample(n=5)
+print("Dataset has {} entries and {} features".format(*df.shape))
+X, y = df.loc[:,:52].values, df.loc[:,53].values
+#print(X.shape,y.shape)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=.1, random_state=42)
+import xgboost as xgb
+dtrain = xgb.DMatrix(X_train, label=y_train)
+dtest = xgb.DMatrix(X_test, label=y_test)
+from sklearn.metrics import mean_absolute_error
+import numpy as np
+# "Learn" the mean from the training data
+mean_train = np.mean(y_train)
+print(y_train,mean_train)
+# Get predictions on the test set
+baseline_predictions = np.ones(y_test.shape) * mean_train
+print(baseline_predictions)
+# Compute MAE
+mae_baseline = mean_absolute_error(y_test, baseline_predictions)
+print("Baseline MAE is {:.2f}".format(mae_baseline))
+params = {
+    # Parameters that we are going to tune.
+    'max_depth':6,
+    'min_child_weight': 1,
+    'eta':.3,
+    'subsample': 1,
+    'colsample_bytree': 1,
+    # Other parameters
+    'objective':'reg:linear',
+}
+params['eval_metric'] = "mae"
+num_boost_round = 999
+model = xgb.train(
+    params,
+    dtrain,
+    num_boost_round=num_boost_round,
+    evals=[(dtest, "Test")],
+    early_stopping_rounds=10
+)
+
+
+
+print("Best MAE: {:.2f} with {} rounds".format(
+                 model.best_score,
+                 model.best_iteration+1))
+cv_results = xgb.cv(
+    params,
+    dtrain,
+    num_boost_round=num_boost_round,
+    seed=42,
+    nfold=5,
+    metrics={'mae'},
+    early_stopping_rounds=10
+)
+print(cv_results)
+print(cv_results['test-mae-mean'].min())
+
+# You can try wider intervals with a larger step between
+# each value and then narrow it down. Here after several
+# iteration I found that the optimal value was in the
+# following ranges.
+gridsearch_params = [
+    (max_depth, min_child_weight)
+    for max_depth in range(9,12)
+    for min_child_weight in range(5,8)
+]
+
+# Define initial best params and MAE
+min_mae = float("Inf")
+best_params = None
+for max_depth, min_child_weight in gridsearch_params:
+    print("CV with max_depth={}, min_child_weight={}".format(
+                             max_depth,
+                             min_child_weight))
+    # Update our parameters
+    params['max_depth'] = max_depth
+    params['min_child_weight'] = min_child_weight
+    # Run CV
+    cv_results = xgb.cv(
+        params,
+        dtrain,
+        num_boost_round=num_boost_round,
+        seed=42,
+        nfold=5,
+        metrics={'mae'},
+        early_stopping_rounds=10
+    )
+    # Update best MAE
+    mean_mae = cv_results['test-mae-mean'].min()
+    boost_rounds = cv_results['test-mae-mean'].argmin()
+    print("\tMAE {} for {} rounds".format(mean_mae, boost_rounds))
+    if mean_mae < min_mae:
+        min_mae = mean_mae
+        best_params = (max_depth,min_child_weight)
+print("Best params: {}, {}, MAE: {}".format(best_params[0], best_params[1], min_mae))
+params['max_depth'] = 10
+params['min_child_weight'] = 6
+gridsearch_params = [
+    (subsample, colsample)
+    for subsample in [i/10. for i in range(7,11)]
+    for colsample in [i/10. for i in range(7,11)]
+]
+
+min_mae = float("Inf")
+best_params = None
+# We start by the largest values and go down to the smallest
+for subsample, colsample in reversed(gridsearch_params):
+    print("CV with subsample={}, colsample={}".format(
+                             subsample,
+                             colsample))
+    # We update our parameters
+    params['subsample'] = subsample
+    params['colsample_bytree'] = colsample
+    # Run CV
+    cv_results = xgb.cv(
+        params,
+        dtrain,
+        num_boost_round=num_boost_round,
+        seed=42,
+        nfold=5,
+        metrics={'mae'},
+        early_stopping_rounds=10
+    )
+    # Update best score
+    mean_mae = cv_results['test-mae-mean'].min()
+    boost_rounds = cv_results['test-mae-mean'].argmin()
+    print("\tMAE {} for {} rounds".format(mean_mae, boost_rounds))
+    if mean_mae < min_mae:
+        min_mae = mean_mae
+        best_params = (subsample,colsample)
+print("Best params: {}, {}, MAE: {}".format(best_params[0], best_params[1], min_mae))
+
+
+params['subsample'] = .8
+params['colsample_bytree'] = 1.
+
+
+# This can take some time…
+
+#The ETA parameter controls the learning rate.
+min_mae = float("Inf")
+best_params = None
+for eta in [.3, .2, .1, .05, .01, .005]:
+    print("CV with eta={}".format(eta))
+    # We update our parameters
+    params['eta'] = eta
+    # Run and time CV
+    cv_results = xgb.cv(
+            params,
+            dtrain,
+            num_boost_round=num_boost_round,
+            seed=42,
+            nfold=5,
+            metrics=['mae'],
+            early_stopping_rounds=10
+          )
+    # Update best score
+    mean_mae = cv_results['test-mae-mean'].min()
+    boost_rounds = cv_results['test-mae-mean'].argmin()
+    print("\tMAE {} for {} rounds\n".format(mean_mae, boost_rounds))
+    if mean_mae < min_mae:
+        min_mae = mean_mae
+        best_params = eta
+print("Best params: {}, MAE: {}".format(best_params, min_mae))
+
